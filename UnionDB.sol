bd1146331b27fceb1d1819fe8aa34314d5916ad8
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-03-01 05:40:51 +0100

    Added more func

--- UnionDB.sol
+++ UnionDB.sol
@@ -63,8 +63,9 @@ contract UnionDB{
             string calldata _nameOfPresident,
             uint _numOfState,
             uint _population,
-            string calldata AmbName,
-            string calldata gender
+            string calldata _AmbName,
+            string calldata _gender,
+            string calldata _nameOfUnion
         )
         external returns(string memory message){
          // string nameOfCountry
@@ -72,8 +73,45 @@ contract UnionDB{
         // uint numOfState;
         // uint population;
         // string name;
-
         // string country;
+        Union storage u = unions[_nameOfUnion];
+        Country storage c = u.countries[_nameOfCountry];
+        require(u.status == true, "No such union!");
+
+        // one liner
+        //Country storage uc = unions[_nameOfUnion].countries[_nameOfCountry];
+
+        c.nameOfCountry = _nameOfCountry;
+        c.nameOfPresident = _nameOfPresident;
+        c.numOfState = _numOfState;
+        c.ambassador.name = _AmbName;
+        c.ambassador.gender = _gender;
+        c.population = _population;
+
+        u.country.push(_nameOfCountry);
+        message = "!Member of the Union creatd";
     }
+
+    function seeUnion(string calldata _nameOfU) external view returns(UnionV2 memory _u){
+        Union storage u = unions[_nameOfU];
+        // string nameOfUnion;
+        // string[] country;
+        // bool status;
+        _u.nameOfUnion = u.nameOfUnion;
+        _u.country = u.country;
+    }  
+
+    function seeAllUnion(string[] memory _nameOfu) external view returns(UnionV2[] memory _u){
+        _u = new UnionV2[](_nameOfu.length);
+        for(uint i = 0; i < _nameOfu.length; i++){
+            Union storage u = unions[_nameOfu[i]];
+            require(u.status == true, "One of your inputs is incorrect");
+            // _u[i] = u.country
+            _u[i].nameOfUnion = u.nameOfUnion;
+            _u[i].country = u.country;
+        }
+    }  
+
+
        
 }     
